83754b368defd753abb18fb4f956bf410b7f5506
Cao Cự Chính <chinhcaocu@gmail.com>
2025-03-26 13:05:41 +0700

    Refactor HomePage component by removing Tilt effect and replacing it with a simpler div structure for improved performance and maintainability. Delete unused Tilt component to streamline the codebase.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,24 +2,11 @@
 import {ImageComparisonImage, ImageComparisonSlider} from "@/components/ui/image-comparison";
 import {ImageComparison} from "@/components/ui/image-comparison";
 import {Spotlight} from "@/components/ui/spotlight";
-import {Tilt} from "@/components/ui/tilt";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen items-center justify-center flex flex-col overflow-x-hidden p-5">
-      <Tilt
-        rotationFactor={6}
-        isRevese
-        style={{
-          transformOrigin: "center center",
-        }}
-        springOptions={{
-          stiffness: 26.7,
-          damping: 4.1,
-          mass: 0.2,
-        }}
-        className="group relative rounded-lg w-[90%] sm:w-[75%] flex items-center justify-center"
-      >
+      <div className="group relative rounded-lg w-[90%] sm:w-[75%] flex items-center justify-center">
         <Spotlight
           className="z-10 from-white/50 via-white/20 to-white/10 blur-2xl"
           size={248}
@@ -46,7 +33,7 @@ export default function HomePage() {
           />
           <ImageComparisonSlider className="bg-white" />
         </ImageComparison>
-      </Tilt>
+      </div>
       <div className="flex flex-col space-y-0.5 pb-0 pt-3 items-center justify-center">
         <h3 className="font-mono font-medium text-zinc-500 dark:text-zinc-400 text-3xl text-center">VTEAM - Vinschool Central Park</h3>
         <p className="text-black dark:text-white text-xl text-center">2024-2025</p>
