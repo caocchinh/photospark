0ac1e8ecb9d189b8667b56ce484f8747311d4174
Cao Cự Chính <chinhcaocu@gmail.com>
2025-03-26 22:49:22 +0700

    Update motion package to version 12.6.2 in client and web projects. Enhance HomePage component with a new ProgressiveBlur effect on image hover for improved user interaction and visual appeal.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -22,9 +22,13 @@ import {SiGithub} from "react-icons/si";
 import {TfiEmail} from "react-icons/tfi";
 import LanguageBar from "@/components/LanguageBar";
 import {useTranslation} from "react-i18next";
+import {useState} from "react";
+import {ProgressiveBlur} from "@/components/ui/progressive-blur";
 
 export default function HomePage() {
   const {t} = useTranslation();
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <div className="min-h-screen items-center justify-center flex overflow-x-hidden p-5 gap-4 flex-col">
       <div className="w-[95%] flex items-center justify-end">
@@ -118,11 +122,28 @@ export default function HomePage() {
                 }}
                 className="flex max-w-full flex-col overflow-hidden border border-zinc-950/10 bg-white dark:border-zinc-50/10 dark:bg-zinc-900"
               >
-                <MorphingDialogImage
-                  src="/cute.jpg"
-                  alt="Cao Cự Chính is the developer of this website. All rights reserved."
-                  className="h-48 w-full object-cover"
-                />
+                <div
+                  className="relative h-max overflow-hidden"
+                  onMouseEnter={() => setIsHover(true)}
+                  onMouseLeave={() => setIsHover(false)}
+                >
+                  <MorphingDialogImage
+                    src="/cute.jpg"
+                    alt="Cao Cự Chính is the developer of this website. All rights reserved."
+                    className="h-48 w-full object-cover"
+                  />
+                  <ProgressiveBlur
+                    className="pointer-events-none absolute bottom-0 left-0 h-[50%] w-full"
+                    blurIntensity={0.5}
+                    animate={isHover ? "visible" : "hidden"}
+                    variants={{
+                      hidden: {opacity: 0},
+                      visible: {opacity: 1},
+                    }}
+                    transition={{duration: 0.2, ease: "easeOut"}}
+                  />
+                </div>
+
                 <div className="flex flex-col grow items-center justify-center px-3 py-2">
                   <MorphingDialogTitle className="text-zinc-950 dark:text-zinc-50  flex flex-wrap text-center  items-center gap-1 justify-center">
                     {t("Developer Information")} <SiMinutemailer size={17} />
